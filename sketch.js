d32d8b754bfc9f0914d0a1fea9746ae5433ce50c
Fakhir Ali <ali.fakhir33@gmail.com>
2020-09-12 11:38:46 +0500

    added stats and working on peer

--- sketch.js
+++ sketch.js
@@ -1,10 +1,34 @@
+
+
+let url;
 let x = 0;
 let y = 0;
-let end = false;
+let end = true;
+let time = 0;
+let totalTime = 0;
+let count = 0;
+
+let width = 1000
+let height = 700;
+
+var peer = new Peer();
+
+
+peer.on('open', function(id) {
+  console.log('My peer ID is: ' + id);
+  
+});
+
+
+
+
+
 
 function setxy(){
-	x = random(100,1400);
-	y = random(100,600);
+	url = getURL();
+	print(url);
+	x = random(50,width-50);
+	y = random(50,height-50);
 	
 	
 }
@@ -13,8 +37,7 @@ function setxy(){
 
 
 function setup() {
-  createCanvas(1500, 700);
-  
+  createCanvas(width, height);
   setxy()
 
   
@@ -23,7 +46,11 @@ function setup() {
 
 function draw() {
   if (end){
-   background(0) ;
+   background(0);
+   textSize(32);
+   text(str(round(totalTime/count,3)), (width/2)-50, height/2);
+   text(str(round(count,3)), (width/2)-50, height/2 + 50);
+
     
   }else{
 	  background(150);
@@ -39,13 +66,17 @@ function draw() {
 function mousePressed(){
   if (end == false){
     if (abs(mouseX - x) < 25 && abs(mouseY - y) < 25){
+		count += 1;
 		setxy()
     }else{
+		totalTime = (millis() - time)/1000;
 		setxy()
 		end = true;
     }
   }else{
-	end = false;
+	  count = 0;
+	  time = millis();
+	  end = false;
   }
   
   
